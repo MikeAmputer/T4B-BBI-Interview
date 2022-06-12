e0733aad69431770f61e86719c7c681098edfc4f
Pavel Kravtsov <p.kravtsov@tools4brokers.com>
2022-06-12 10:03:09 +0300

    Code sample 1

---
+++ CodeSamples/CodeSample-1/Program.cs
@@ -0,0 +1,16 @@
+﻿var task1 = Task.Run(() => Foo.Instance);
+var task2 = Task.Run(() => Foo.Instance);
+Console.WriteLine(task1.Result == task2.Result);
+
+class Foo
+{
+    private static Foo? _instance;
+    public static Foo Instance
+    {
+        get
+        {
+            _instance ??= new();
+            return _instance;
+        }
+    }
+}
