503887c8059d05aba96de78b137b14d2da8536bf
Pavel Kravtsov <p.kravtsov@tools4brokers.com>
2022-06-12 20:02:09 +0300

    Add code sample 4

---
+++ CodeSamples/CodeSample-4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("users")]
+class User
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
+
+[Table("deals")]
+class Deal
+{
+    public long Id { get; set; }
+    public long UserId { get; set; }
+    public virtual User? User { get; set; }
+    public decimal Profit { get; set; }
+}
