f793291c70724ff123c267d9517517e125dc0f47
Pavel Kravtsov <p.kravtsov@tools4brokers.com>
2022-06-12 19:22:26 +0300

    Add code sample 3

---
+++ CodeSamples/CodeSample-3/Program.cs
@@ -0,0 +1,20 @@
+﻿class Foo
+{
+    public void DoSomething()
+    {
+        Console.WriteLine("Doing the foo do.");
+    }
+}
+
+class Bar
+{
+
+}
+
+interface IVariant<in F, out B>
+    where F : Foo
+    where B : Bar
+{
+    private F ConvertToFoo<T>(T item) where T : B
+    public void DoSomething<T>(T item) where T : B => ConvertToFoo(item).DoSomething();
+}
